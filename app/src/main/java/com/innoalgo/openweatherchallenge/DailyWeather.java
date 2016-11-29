5f3aa921d92351377906937b7c4279e2a7b19f3b
Corey Murphy <c.murphy360@gmail.com>
2016-11-29 13:42:51 -0800

    Flushing out DailyWeather Class to hold all required data.

--- app/src/main/java/com/innoalgo/openweatherchallenge/DailyWeather.java
+++ app/src/main/java/com/innoalgo/openweatherchallenge/DailyWeather.java
@@ -1,16 +1,49 @@
 package com.innoalgo.openweatherchallenge;
 
+import java.io.Serializable;
+import java.util.Date;
+
 /**
  * Created by Corey on 11/29/2016.
  */
 
-public class DailyWeather {
-    
+public class DailyWeather implements Serializable {
+
+    private Date forecastDate;
     private String weatherIconText;
     private String dayText;
     private String weatherText;
-    private String currentTempText;
-    private String lowTempText;
+    private Double currentTemp;
+    private Double minTemp;
+    private Double maxTemp;
+    private Double humidity;
+    private Double pressure;
+    private String cloudText;
+    private String cloudDescription;
+    private String iconText;
+    private int windSpeed;
+    private int windDirection;
+
+    public DailyWeather(Date d, Double temp, Double tempMin, Double tempMax, Double humidity,
+                        Double pressure, String cloudText, String cloudDescription, String iconText,
+                        int windSpeed, int windDirection) {
+
+        this.forecastDate = d;
+        this.currentTemp = temp;
+        this.minTemp = tempMin;
+        this.maxTemp = tempMax;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        this.cloudText = cloudText;
+        this.cloudDescription = cloudDescription;
+        this.iconText = iconText;
+        this.windSpeed = windSpeed;
+        this.windDirection = windDirection;
+    }
+
+    public DailyWeather() {
+
+    }
 
     //TODO Create Constructor
 
@@ -26,11 +59,8 @@ public class DailyWeather {
         return weatherText;
     }
 
-    public String getCurrentTempText() {
-        return currentTempText;
+    public Double getCurrentTemp() {
+        return currentTemp;
     }
 
-    public String getLowTempText() {
-        return lowTempText;
-    }
 }
